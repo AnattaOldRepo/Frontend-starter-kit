f0dc0ba524cf760b756e22aa5c97755ad4bcf812
monkviper <surinder@anattadesign.com>
2016-07-28 00:52:39 +0530

    fix formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,52 @@
 var debug = process.env.NODE_ENV !== "production";
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var postcssUtilities  = require('postcss-utilities');
-var postcssInitial    = require('postcss-initial');
-var postcssImport     = require('postcss-import');
-var postcssShort      = require('postcss-short');
-var autoprefixer      = require('autoprefixer');
-var webpack           = require('webpack');
-var precss            = require('precss');
-
-
+var postcssUtilities = require('postcss-utilities');
+var postcssInitial = require('postcss-initial');
+var postcssImport = require('postcss-import');
+var postcssShort = require('postcss-short');
+var autoprefixer = require('autoprefixer');
+var webpack = require('webpack');
+var precss = require('precss');
 
 
 module.exports = {
 
-  devtool: debug ? "inline-sourcemap" : null,
-  entry: "./src/index.js",
+    devtool: debug ? "inline-sourcemap" : null,
+    entry: "./src/index.js",
 
-  output: {
-    path: "./build",
-    filename: "app.js"
-  },
+    output: {
+        path: "./build",
+        filename: "app.js"
+    },
 
-  resolve: {
-    alias: {
-      images: "./src/images"
-    }
-  },
+    resolve: {
+        alias: {
+            images: "./src/images"
+        }
+    },
 
-  module: {
+    module: {
+
         loaders: [
 
-            { 
-              test: /\.css$/, 
-              loader: ExtractTextPlugin.extract( 'css-loader!resolve-url!postcss-loader' )
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('css-loader!resolve-url!postcss-loader')
             },
 
             {
-              test: /\.(jpe?g|png|gif|svg)$/,
-              loader: 'url-loader?limit=10000&name=images/[name].[ext]'
+                test: /\.(jpe?g|png|gif|svg)$/,
+                loader: 'url-loader?limit=10000&name=images/[name].[ext]'
             }
         ]
-  },
 
-   postcss: function(webpack) {
+    },
+
+    postcss: function(webpack) {
         return [
-            postcssImport({ addDependencyTo: webpack }), // Must be first item in list
+            postcssImport({
+                addDependencyTo: webpack
+            }), // Must be first item in list
             postcssUtilities,
             postcssShort,
             postcssInitial,
@@ -53,12 +55,15 @@ module.exports = {
         ];
     },
 
-  plugins: debug ? [
-    new ExtractTextPlugin("app.css")
-  ] : [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
-  ],
+    plugins: debug ? [
+        new ExtractTextPlugin("app.css")
+    ] : [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            mangle: false,
+            sourcemap: false
+        }),
+    ],
 
 };
