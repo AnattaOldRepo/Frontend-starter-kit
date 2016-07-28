6c7144b2be79960c49f18057b542805b6e1a016c
monkviper <surinder@anattadesign.com>
2016-07-28 16:23:37 +0530

    add jslint preloader

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,14 @@ module.exports = {
 
     module: {
 
+        preLoaders: [
+            {
+                test: /\.js$/, // include .js files
+                exclude: /node_modules/, // exclude any and all files in the node_modules folder
+                loader: "jshint-loader"
+            }
+        ],
+
         loaders: [
 
             {
