a2d4da59e04882b3560f7921205e40a13e54d46e
Hussain <hussain@Ashiques-MacBook-Pro.local>
2016-07-28 15:37:04 +0530

    Window Resize function

--- src/js/load/index.js
+++
@@ -1,6 +0,0 @@
-/*global window*/
-var $ = require("jquery");
-$(window).on('load', function () {
-    'use strict';
-    // executes when complete page is fully loaded, including all frames, objects and images 
-});
