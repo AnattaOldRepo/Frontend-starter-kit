fdc65d8d2d342980cb0f910b48f19a481ca3c5ac
Hussain <hussain@Ashiques-MacBook-Pro.local>
2016-07-28 17:32:52 +0530

    Accordion Function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 require('./js/modal/index.js');
+require('./js/accordion/index.js');
 require("./css/index.css");
--- src/js/accordion/index.js
+++ src/js/accordion/index.js
@@ -1,5 +1,15 @@
 var jQuery = require("jquery");
 (function($) {
 
+  	$("#accordion > li > div").click(function(){
+
+  	if(false === $(this).next().is(':visible')) {
+  		$('#accordion ul').slideUp(300);
+  	}
+  	$(this).next().slideToggle(300);
+  
+});
+
+$('#accordion ul:eq(0)').show();
 
 })(jQuery);
