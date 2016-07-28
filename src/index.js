956bde09fb3d6e98129bf654c86efd26e3393f94
Hussain <hussain@Ashiques-MacBook-Pro.local>
2016-07-28 15:19:56 +0530

    keep modal only

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
-
-require('./js/hamburger/index.js');
-require("./css/index.css");
-require('./js/resize/index.js');
-require('./js/load/index.js');
-require('./js/toggle/index.js');
 require('./js/modal/index.js');
-require('./js/valid/index.js');
-require("./css/test.css");
+require("./css/index.css");
