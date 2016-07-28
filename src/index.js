a62c5ea70ab8e3e082bc7d655cfd93a309295174
Hussain <hussain@Ashiques-MacBook-Pro.local>
2016-07-29 00:11:34 +0530

    Load More Function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-require('./js/modal/index.js');
+require('./js/loadmore/index.js');
 require("./css/index.css");
---
+++ src/js/loadmore/index.js
@@ -0,0 +1,14 @@
+var jQuery = require("jquery");
+(function ($) {
+    size_li = $("#myList li").length;
+    x = 3;
+    $('#myList li:lt('+ x +')').show();
+    $('#loadMore').click(function () {
+        x= (x + 5 <= size_li) ? x + 5 : size_li;
+        $('#myList li:lt('+ x +')').show();
+    });
+    $('#showLess').click(function () {
+        x=(x - 5 < 0) ? 3 : x - 5;
+        $('#myList li').not(':lt('+ x +')').hide();
+    });
+}(jQuery));
